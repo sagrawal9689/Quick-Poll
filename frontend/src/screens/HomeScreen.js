45482e87e5cb557d7953711d3dc534eb0063e7bb
unknown <sagrawal9689@gmail.com>
2021-10-01 22:25:21 +0530

    Home Screen form and styling added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Container, Form } from 'react-bootstrap'
 
 const HomeScreen = () => {
     return (
-        <div>
-            HomeScreen
+        <>
+        
+        <div style={{ textAlign: "center", fontSize: "57px"}} className="m-5">
+          Quick Poll
+        <div style={{ textAlign: "center", fontSize: "20px" }}>Create Polls Quickly.</div>
         </div>
+
+        <main>
+        <Form style={{backgroundColor: "#F2F2F2"}} className="formMain">
+            <h1>Create Poll</h1>
+            <Form.Group>
+              <Form.Label>Poll Question</Form.Label>
+              <Form.Control as="textarea" rows={3}/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Option 1</Form.Label> 
+              <Form.Control type="text" placeholder="Option 1"/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Option 2</Form.Label>
+              <Form.Control type="text" placeholder="Option 2"/>
+            </Form.Group>
+            
+        </Form>
+        </main>
+        </>
     )
 }
 
