c195254796d0a9c037ac043e1f04428b09848509
unknown <sagrawal9689@gmail.com>
2021-10-04 20:00:25 +0530

    added axios post request for poll creation in homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomeScreen/>
+      <hr style={{height: "10px", color: "#A020F0", margin: "0"}}/>
+      <Router>
+      <Route path="/" exact component={HomeScreen}/>
+      </Router>
     </>
   );
 }
