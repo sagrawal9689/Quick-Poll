174b7dc1f0332f035c1cf4d50006876a55cac133
unknown <sagrawal9689@gmail.com>
2021-10-07 23:04:51 +0530

    Added backend route for updating options count

--- backend/routes/pollRoutes.js
+++ backend/routes/pollRoutes.js
@@ -2,10 +2,11 @@ import express from 'express'
 const router = express.Router()
 import {
   createPoll,
-  getPoll
+  getPoll,
+  increaseCount
 } from '../controllers/pollController.js'
 
 router.route('/').post(createPoll)
 router.route('/:id').get(getPoll)
-
+router.route('/increase').patch(increaseCount)
 export default router
